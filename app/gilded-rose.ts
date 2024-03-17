e4369fea004234b53bd7a6025f3f95c8956d0844
Guilherme-07062002 <guilherme762002@gmail.com>
2024-03-17 10:23:20 -0300

    refactor: condense conditionals

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -20,46 +20,34 @@ export class GildedRose {
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       const item = this.items[i];
-      if (item.name == 'Aged Brie' || item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+      if (item.name == 'Aged Brie') {
+        if (item.quality < 50) item.quality += 1
+        
+      } else if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
         if (item.quality < 50) {
-          item.quality = item.quality + 1
+        item.quality += 1
           if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
+              if (item.quality < 50) item.quality += 1
             }
             if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
+              if (item.quality < 50) item.quality += 1
             }
           }
         }
-      } else {
-        if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.quality = item.quality - 1
-          }
-        }
-        
+      } else if (item.quality > 0) {
+        if (item.name != 'Sulfuras, Hand of Ragnaros') item.quality -= 1
       }
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
-        item.sellIn = item.sellIn - 1;
-      }
+      if (item.name != 'Sulfuras, Hand of Ragnaros') item.sellIn -= 1
       if (item.sellIn < 0) {
         if (item.name == 'Aged Brie') {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1
-          }
+          if (item.quality < 50) item.quality = item.quality + 1
         } else {
           if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-            item.quality = item.quality - item.quality
+            item.quality -= item.quality
           } else {
             if (item.quality > 0) {
-              if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                item.quality = item.quality - 1
-              }
+              if (item.name != 'Sulfuras, Hand of Ragnaros') item.quality -= 1
             }
           }
         }
